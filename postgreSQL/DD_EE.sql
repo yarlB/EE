03ae7de1269745daca08c310071d19a3cdc8d8d7
yarl baudig <yar-baudig@mailoo.org>
2014-05-07 23:23:37 +0200

    pg

--- postgreSQL/DD_EE.sql
+++ postgreSQL/DD_EE.sql
@@ -10,9 +10,42 @@ DROP TABLE IF EXISTS objets CASCADE;
 CREATE TABLE objets (
        id_objet SERIAL,
        nom VARCHAR(20),
-       precond INTEGER[],
        effet VARCHAR(50),
        CONSTRAINT pk_objets PRIMARY KEY (id_objet)
+);
+
+
+DROP TABLE IF EXISTS predicats CASCADE;
+CREATE TABLE predicats (
+       id_predicat SERIAL,
+       id_substitution INTEGER, --substitution(s) pour la(les) quelle(s) le prédicat est vrai
+       CONSTRAINT pk_predicats PRIMARY KEY (id_predicat)
+);
+
+DROP TABLE IF EXISTS constantes CASCADE;
+CREATE TABLE constantes (
+       id_constante SERIAL,
+       symbole VARCHAR(20),
+       CONSTRAINT pk_constantes PRIMARY KEY (id_constante)
+);
+
+DROP TABLE IF EXISTS preconditions CASCADE;
+CREATE TABLE preconditions (
+       id_precondition SERIAL,
+       id_predicat INTEGER,
+       id_substitution INTEGER,
+       CONSTRAINT pk_preconditions PRIMARY KEY(id_precondition),
+       CONSTRAINT fk_preconditions_id_predicat FOREIGN KEY (id_predicat) REFERENCES predicat(id_predicat),
+       CONSTRAINT fk_preconditions_id_substitution FOREIGN KEY (id_substitution) REFERENCES substitutions(id_substitution)
+);
+
+DROP TABLE IF EXISTS objet_preconditionss CASCADE;
+CREATE TABLE objet_preconds (
+       id_objet INTEGER,
+       id_precondition INTEGER,
+       CONSTRAINT fk_object_preconditions_id_objet FOREIGN KEY (id_objet) REFERENCES objets(id_objet),
+       CONSTRAINT fk_object_preconditions_id_precondition FOREIGN KEY (id_precondition) 
+       REFERENCES preconditions(id_precondition)
 );
 
 -- FAIRE index
@@ -22,22 +55,6 @@ CREATE TABLE zone_objets (
        id_objet INTEGER,
        CONSTRAINT fk_zone_objets_id_zone FOREIGN KEY (id_zone) REFERENCES zones(id_zone),
        CONSTRAINT fk_zone_objets_id_objet FOREIGN KEY (id_objet) REFERENCES objets(id_objet)
-);
-
-
-DROP TABLE IF EXISTS predicats CASCADE;
-CREATE TABLE predicats (
-       id_predicat SERIAL,
-       arite INT,
-       vraipour INTEGER[],
-       CONSTRAINT pk_predicats PRIMARY KEY (id_predicat)
-);
-
-DROP TABLE IF EXISTS constantes CASCADE;
-CREATE TABLE constantes (
-       id_constante SERIAL,
-       symbole VARCHAR(20),
-       CONSTRAINT pk_constantes PRIMARY KEY (id_constante)
 );
 
 -- DEFINIR volatilité minimum
